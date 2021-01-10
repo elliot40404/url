66ccb35f1bbf18f787ec565c30a573f3325ed6b0
elliot404 <fortesting40404@gmail.com>
2021-01-10 21:30:32 +0530

    works

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Url = require('./models/url');
+const app = express();
+const port = process.env.PORT || 80;
+const ip = "192.168.0.8";
+
+mongoose.connect('mongodb://localhost/url', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+});
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+    res.render('index', { url: new Url() });
+});
+
+app.get('/log', async (req, res) => {
+    const urls = await Url.find().sort({ createdAt: 'desc' })
+    res.render('log', { urls: urls });
+});
+
+app.post('/url', async (req, res) => {
+    let url = new Url({
+        link: req.body.link
+    });
+    try {
+        url = await url.save();
+        res.render('index', { url: url });
+    } catch (e) {
+        res.redirect('/');
+    }
+});
+
+app.get('/:slug', async (req, res) => {
+    try {
+        const red = await Url.findOne({ slug: req.params.slug });
+        res.redirect(red.link);
+    } catch (e) {
+        res.redirect('/');
+    }
+});
+
+app.listen(port, ip);
