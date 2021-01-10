07035cacdc620cc4a12ea5b983ebbe299b58d09c
elliot40404 <fortesting40404@gmail.com>
2021-01-10 21:46:59 +0530

    updated

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Url = require('./models/url');
 const app = express();
-const port = process.env.PORT || 80;
-const ip = "192.168.0.8";
+const port = process.env.PORT || 3000;
+// const ip = "192.168.0.6";
 
 mongoose.connect('mongodb://localhost/url', {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
 
 app.set('view engine', 'ejs');
@@ -18,12 +20,12 @@ app.get('/', (req, res) => {
     res.render('index', { url: new Url() });
 });
 
-app.get('/log', async (req, res) => {
+app.get('/log', async(req, res) => {
     const urls = await Url.find().sort({ createdAt: 'desc' })
     res.render('log', { urls: urls });
 });
 
-app.post('/url', async (req, res) => {
+app.post('/url', async(req, res) => {
     let url = new Url({
         link: req.body.link
     });
@@ -35,7 +37,7 @@ app.post('/url', async (req, res) => {
     }
 });
 
-app.get('/:slug', async (req, res) => {
+app.get('/:slug', async(req, res) => {
     try {
         const red = await Url.findOne({ slug: req.params.slug });
         res.redirect(red.link);
@@ -44,4 +46,5 @@ app.get('/:slug', async (req, res) => {
     }
 });
 
-app.listen(port, ip);
+// app.listen(port, ip);
+app.listen(port);
